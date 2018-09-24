21d829e6d19241df2a48224b5b1ffab69f7244c8
ebarq-app <43491608+ebarq-app@users.noreply.github.com>
2018-09-25 00:31:53 +1000

    WPS2

--- EBARQWebApp/urls.py
+++ EBARQWebApp/urls.py
@@ -8,12 +8,10 @@ urlpatterns = [
 
     url(r'^login/$', views.login_view, name='login'),
     url(r'^signup/$', views.signup, name='signup'),
-<<<<<<< HEAD
-=======
+
     url(r'^dashboard/', views.dashboard, name='dashboard'),
     url(r'^horse_add/',views.horse_add_view, name='horse_add'),
 
->>>>>>> 607c5512be081ffd2c64abb305ddc1a7efdd733b
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
 
