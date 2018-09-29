cfa13bf46c42aa2a8cb3a8f4cc26e1a1c8a879d9
damaja112 <william_smitty@Hotmail.com>
2018-09-29 15:26:31 +1000

    DB SQLite adjhustment

--- EBARQ/settings.py
+++ EBARQ/settings.py
@@ -73,8 +73,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'EBARQ.wsgi.application'
 
 
-# # Database
-# # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 # if 'RDS_DB_NAME' in os.environ:
 #     DATABASES = {
 #         'default': {
@@ -87,12 +87,13 @@ WSGI_APPLICATION = 'EBARQ.wsgi.application'
 #         }
 #     }
 # else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }       
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -129,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'dashboard'
