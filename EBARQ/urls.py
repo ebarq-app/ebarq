0d3a34d847739b644335bc81e804d8c9868d85e5
damaja112 <william_smitty@Hotmail.com>
2018-10-03 13:24:11 +1000

    revert

--- EBARQ/urls.py
+++ EBARQ/urls.py
@@ -1,34 +1,22 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+"""EBARQ URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include,url
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^signup/$', views.signup, name='signup'),
-
-    url(r'^dashboard/', views.dashboard, name='dashboard'),
-    url(r'^horse_add/',views.horse_add_view, name='horse_add'),
-url(r'^horse_add_view_new/',views.horse_add_view_new, name='horse_add_new'),
-
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
-
-    url(r'^dashboard/', views.dashboard, name='dashboard'),
-    url(r'^ebarqdashboard/',views.ebarqdashboard, name='ebarqdashboard'),
-    url(r'^userprofile/',views.userprofile, name='userprofile'),
-    url(r'^editprofile/',views.editprofile, name='editprofile'),
-    url(r'^horseprofile/',views.horseprofile, name='horseprofile'),
-url(r'^horse_inDepth/',views.horse_inDepth, name='horse_inDepth'),
-    # url(r'^addhorse/', views.addhorse, name='addhorse'),
-
-    url(r'^addperformance/',views.addperformance, name='addperformance'),
-    url(r'^addreminder/',views.addreminder, name='addreminder'),
-    url(r'^horseReminders/',views.horseReminders, name='horseReminders'),
-     url(r'^setting/',views.setting, name='setting')
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('EBARQWebApp.urls'))
+]
