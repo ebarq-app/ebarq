5903738e2570da75637e5bbe31c24b7b5dc0e65d
damaja112 <william_smitty@Hotmail.com>
2018-10-11 17:48:16 +1100

    please

--- EBARQWebApp/commands/createsu.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        if not User.objects.filter(username="ebarq-admin").exists():
-            User.objects.create_superuser("ebarq-admin", "ebarq11@gmail.com", "ebarq-admin")
